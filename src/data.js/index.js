eb1213ab4a826c371eaf161c7f5a0832d007a175
Atul Kumar Hande <handeatul03a@gmail.com>
2022-04-15 01:17:16 +0530

    feat: added assets & categories list

---
+++ src/data.js/index.js
@@ -0,0 +1,30 @@
+export const categories = [
+  {
+    id: 1,
+    name: "Bed",
+    image:
+      "https://github.com/AtulHande03a/Ecom-screens/blob/cc056c565e5687442a08aeb6c5b745fd1aca3851/assets/images/bed.jpg",
+    slug: "bed",
+  },
+  {
+    id: 2,
+    name: "Sofa Set",
+    image:
+      "https://github.com/AtulHande03a/Ecom-screens/blob/cc056c565e5687442a08aeb6c5b745fd1aca3851/assets/images/sofa.jpg",
+    slug: "sofa-set",
+  },
+  {
+    id: 3,
+    name: "Chair",
+    image:
+      "https://github.com/AtulHande03a/Ecom-screens/blob/cc056c565e5687442a08aeb6c5b745fd1aca3851/assets/images/office-chair.jpg",
+    slug: "chair",
+  },
+  {
+    id: 4,
+    name: "Coffee Table",
+    image:
+      "https://github.com/AtulHande03a/Ecom-screens/blob/cc056c565e5687442a08aeb6c5b745fd1aca3851/assets/images/office-chair.jpg",
+    slug: "coffee-table",
+  },
+];
