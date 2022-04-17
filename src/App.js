b8de1b7d2236d0bc1b931004aa0fec15df8098fc
Atul Kumar Hande <handeatul03a@gmail.com>
2022-04-18 03:19:15 +0530

    feat: added wishlist to cart-context & cartReducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import { Footer, Navbar } from "components";
 import { Routes } from "routes/Routes";
 import "./styles/styles.css";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
     <>
+      <ToastContainer
+        theme="colored"
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        draggable
+        pauseOnHover
+      />
       <Navbar />
       <Routes />
       <Footer />
